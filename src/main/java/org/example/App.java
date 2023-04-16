88a62d06cc0cc0059408cb79c0185981ee76e70b
fangkaif <837220583@qq.com>
2023-04-17 05:03:00 +0800

    test2

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println("test1");
+        System.out.println("test2");
         System.out.println( "Hello World!" );
     }
 }
